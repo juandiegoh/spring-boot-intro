4aeaaae3fe72ad4e949d6e57dbd7ec20f6732b00
Juan Diego Herenu <jdherenu@gmail.com>
2014-10-27 23:25:53 -0300

    Added Spock Integration Spec

--- share_phrase/src/test/groovy/com/jherenu/phrases/controllers/PhraseControllerSpec.groovy
+++ share_phrase/src/test/groovy/com/jherenu/phrases/controllers/PhraseControllerSpec.groovy
@@ -1,51 +1,150 @@
 package com.jherenu.phrases.controllers
 
+import com.jherenu.phrases.Application
+import com.jherenu.phrases.domain.Phrase
+import com.jherenu.phrases.repository.PhraseRepository
+import groovyx.net.http.HttpResponseException
+import groovyx.net.http.RESTClient
+import net.sf.json.JSONArray
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.test.IntegrationTest
 import org.springframework.boot.test.SpringApplicationContextLoader
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.web.client.RestTemplate
 import spock.lang.Specification
 
-@ContextConfiguration(loader = SpringApplicationContextLoader.class)
-@IntegrationTest("server.port:9000")
+@ContextConfiguration(loader = SpringApplicationContextLoader.class, classes = Application.class)
+@WebAppConfiguration
+@IntegrationTest("server.port:0")
 class PhraseControllerSpec extends Specification {
 
-//    @Shared
-//    ConfigurableApplicationContext context
-//
-//    void setupSpec() {
-//        Future future = Executors
-//                .newSingleThreadExecutor().submit(
-//                new Callable() {
-//                    @Override
-//                    public ConfigurableApplicationContext call() throws Exception {
-//                        return (ConfigurableApplicationContext) SpringApplication
-//                                .run(Application.class)
-//                    }
-//                })
-//        context = future.get(60, TimeUnit.SECONDS)
-//    }
-//
-//    void cleanupSpec() {
-//        if (context != null) {
-//            context.close()
-//        }
-//    }
+    @Autowired
+    PhraseRepository repository
+
+    Phrase serONoSer
+    Phrase toBeOrNotToBe
+    Phrase youCompleteMe
 
     @Value('${local.server.port}')
-    int port
+    int thePort
 
-    void "Do my test"() {
-        setup:
-        println port
-        ResponseEntity entity = new RestTemplate().getForEntity("http://localhost:9000/phrases", String.class)
+    RESTClient client
 
-        expect:
-        entity.statusCode == HttpStatus.OK
-        entity.body == 'No Greetings from Spring Boot!'
+    void setup() {
+        serONoSer = new Phrase(language: 'spanish', phrase: 'Ser o no ser')
+        toBeOrNotToBe = new Phrase(language: 'english', phrase: 'To be or not to be')
+        youCompleteMe = new Phrase(language: 'english', phrase: 'You complete me')
+
+        repository.deleteAll();
+        repository.save([serONoSer, toBeOrNotToBe, youCompleteMe]);
+
+        this.client = new RESTClient( "http://localhost:${thePort}/" )
+    }
+
+    void "can get phrase"() {
+        given:
+        def phraseId = serONoSer.getId()
+
+        when:
+        def resp = client.get([path: "/phrases/${phraseId}"])
+
+        then:
+        with(resp) {
+            status == 200
+            contentType == "application/json"
+        }
+
+        with(resp.data) {
+            language == "spanish"
+            phrase == "Ser o no ser"
+            id == phraseId
+        }
+    }
+
+    void "if phrase does not exist return 404"() {
+        given:
+        def notFoundPhraseId = "NotAnId"
+
+        when:
+        client.get([path: "/phrases/${notFoundPhraseId}"])
+
+        then:
+        def e = thrown(HttpResponseException)
+        with(e.response) {
+            status == HttpStatus.NOT_FOUND.value()
+        }
+    }
+
+    void "can get all phrases"() {
+        when:
+        ResponseEntity resp = new RestTemplate().getForEntity("http://localhost:${thePort}/phrases", String.class)
+
+        then:
+        with(resp) {
+            statusCode == HttpStatus.OK
+            headers.getContentType() includes(MediaType.APPLICATION_JSON)
+        }
+
+        with(resp.body) {
+            def json = JSONArray.fromObject(it)
+            json.phrase.every { it in ['Ser o no ser', 'To be or not to be', 'You complete me'] }
+        }
+    }
+
+    void "can get by language"() {
+        given:
+
+        when:
+        def resp = client.get([path: "/phrases/find", query: [language: 'english']])
+
+        then:
+        with(resp) {
+            status == 200
+            contentType == "application/json"
+        }
+
+        with(resp.data) {
+            def json = JSONArray.fromObject(it)
+            json.phrase.every { it in ['To be or not to be', 'You complete me'] }
+            json.phrase.every { it != 'Ser o no ser' }
+        }
+    }
+
+    void canGetByAnotherLanguage() {
+        given:
+
+        when:
+        def resp = client.get([path: "/phrases/find", query: [language: 'spanish']])
+
+        then:
+        with(resp) {
+            status == 200
+            contentType == "application/json"
+        }
+
+        with(resp.data) {
+            def json = JSONArray.fromObject(it)
+            !json.phrase.any { it in ['To be or not to be', 'You complete me'] }
+            json.phrase.every { it == 'Ser o no ser' }
+        }
+    }
+
+    void shouldReturn404WhenLanguageDoesNotMatchAnyPhrase() {
+        given:
+        def unknownLanguage = "geringoso"
+
+        when:
+        client.get([path: "/phrases/find", query: [language: "$unknownLanguage"]])
+
+        then:
+        def e = thrown(HttpResponseException)
+        with(e.response) {
+            status == HttpStatus.NOT_FOUND.value()
+        }
     }
 }
